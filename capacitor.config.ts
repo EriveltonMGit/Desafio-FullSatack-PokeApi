bcc26d63ea2a1b450f5d8a5564e4ceac586de719
EriveltonMGit <eriveltonprojetos.git@gmail.com>
2025-06-16 09:48:26 -0300

    feat: Initial Ionic Angular project setup with tabs template and standalone components

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Desafio-FullSatack-PokeApi',
+  webDir: 'www'
+};
+
+export default config;
